2bbba1e3f45fa3656b7705f1f4f75eb5ec0780e8
qbtin05 <162964029+qbtin05@users.noreply.github.com>
2026-02-06 02:09:36 +0800

    Update diy.sh script content

--- devices/rockchip_armv8/diy.sh
+++ devices/rockchip_armv8/diy.sh
@@ -18,11 +18,10 @@ sed -i "/KernelPackage,ptp/d" package/kernel/linux/modules/other.mk
 rm -rf target/linux/rockchip/armv8/base-files/etc/uci-defaults/13_opkg_update package/feeds/kiddin9/pcat-manager package/feeds/kiddin9/*_QMI_WWAN
 
 # Comment out package replacements that use non-existent packages in OpenWRT 25.12
-# kmod-r8169 and wpad-basic-mbedtls don't exist - keep the original packages
+# kmod-r8169 doesn't exist - keep the original packages
 # sed -i -e 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
-# sed -i -e 's,wpad-openssl,wpad-basic-mbedtls,g' target/linux/rockchip/image/armv8.mk
 # Instead, use packages that actually exist in OpenWRT 25.12:
-sed -i -e 's,wpad-openssl,wpad-mbedtls,g' target/linux/rockchip/image/armv8.mk
+sed -i -e 's,wpad-openssl,wpad-basic-mbedtls,g' target/linux/rockchip/image/armv8.mk
 # Note: kmod-r8168 is kept as-is since kmod-r8169 doesn't exist
 
 # Remove references to non-existent packages in device profiles
@@ -31,7 +30,7 @@ sed -i -e 's/kmod-r8125-rss/kmod-r8125/g' target/linux/rockchip/image/armv8.mk
 sed -i -e 's/kmod-ata-ahci-dwc//g' target/linux/rockchip/image/armv8.mk
 sed -i -e 's/kmod-usb-serial-option//g' target/linux/rockchip/image/armv8.mk
 sed -i -e 's/luci-app-gpsysupgrade//g' target/linux/rockchip/image/armv8.mk
-sed -i -e 's/wpad-basic-mbedtls/wpad-mbedtls/g' target/linux/rockchip/image/armv8.mk
+sed -i -e 's/wpad-mbedtls/wpad-basic-mbedtls/g' target/linux/rockchip/image/armv8.mk
 sed -i -e 's/kmod-r8169/kmod-r8168/g' target/linux/rockchip/image/armv8.mk
 
 # Additional missing packages from error logs
@@ -62,8 +61,8 @@ sed -i -e 's/pcat-manager//g' target/linux/rockchip/image/armv8.mk
 sed -i -e 's/kmod-ath10k-sdio//g' target/linux/rockchip/image/armv8.mk
 sed -i -e 's/kmod-ath10k //g' target/linux/rockchip/image/armv8.mk
 
-# Replace wpad with wpad-mbedtls where it appears alone
-sed -i -e 's/ wpad / wpad-mbedtls /g' target/linux/rockchip/image/armv8.mk
+# Replace wpad with wpad-basic-mbedtls where it appears alone
+sed -i -e 's/ wpad / wpad-basic-mbedtls /g' target/linux/rockchip/image/armv8.mk
 
 # Clean up any double spaces left by removals
 sed -i -e 's/  \+/ /g' target/linux/rockchip/image/armv8.mk
